34735c6b50d04c3186da56acb836e928e2aae67b
Illkwon Ha <haik0925@gmail.com>
2019-12-15 15:56:40 -0800

    initial setup for server project

---
+++ my_blog_server/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace my_blog_server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
