07da7c70c2470890f35929cd2b8dee191159681a
Illkwon Ha <haik0925@gmail.com>
2019-12-15 15:45:36 -0800

    initial project setup for frontend project

---
+++ my_blog_app/webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path')
+
+module.exports = {
+    entry: path.join(__dirname, 'src', 'app.tsx'), // First we specify an application entry point
+    output: { // Up next is the output location for our built bundle.
+        path: path.join(__dirname, 'dist'),
+        filename: path.join('build', 'app.js')
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js'] // Next we tell Webpack to support .ts and .tsx file extensions along with the original .js extension.
+    },
+    module: {
+        rules: [
+            // Finally we tell webpack that for .ts and .tsx files, it should use ts-loader.
+            { test: /\.tsx?$/, loader: 'ts-loader' }
+        ]
+    }
+}
