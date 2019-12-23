d50f2c28074dad3e8bec76a087798033b840ff6f
Illkwon Ha <haik0925@gmail.com>
2019-12-22 18:18:52 -0800

    working on BlogSideBar

--- my_blog_app/src/BlogSideBar.tsx
+++ my_blog_app/src/BlogSideBar.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import "./BlogSideBar.css";
 
 interface BlogSideBarProps {}
 
@@ -16,151 +17,70 @@ export class BlogSideBar extends React.Component<
 
   render() {
     return (
-      <div>
-        <div className="page-wrapper">
-          <div className="sidebar-wrapper">
-            <ul className="sidebar-nav">
-              <li className="sidebar-brand">
-                <a href="#">Zeta</a>
-              </li>
-              <li>
-                <a href="#">Menu 1</a>
-              </li>
-              <li>
-                <a href="#">Menu 2</a>
-              </li>
-              <li>
-                <a href="#">Menu 3</a>
-              </li>
-              <li>
-                <a href="#">Menu 4</a>
-              </li>
-              <li>
-                <a href="#">Menu 5</a>
-              </li>
-              <li>
-                <a href="#">Menu 6</a>
-              </li>
-              <li>
-                <a href="#">Menu 7</a>
-              </li>
-              <li>
-                <a href="#">Menu 8</a>
-              </li>
-              <li>
-                <a href="#">Menu 9</a>
-              </li>
-            </ul>
+      <div className="wrapper">
+        <nav id="sidebar">
+          <div>
+            <h3>SidebarHeader</h3>
           </div>
-          <div id="page-content-wrapper">
-            <div className="container-fluid">
-              <h1>Simple sidebar</h1>
-              <p>
-                Scrollbar appears when size of contents exceeds length of page
-              </p>
-            </div>
-          </div>
-        </div>
+          <ul className="list-unstyled">
+            <li>hahaha</li>
+          </ul>
+        </nav>
         <div>
-          <nav className="navbar-nav flex-column">
-            <a className="nav-link active" href="#">
-              Active
-            </a>
-            <a className="nav-link" href="#">
-              Link
-            </a>
-            <a className="nav-link" href="#">
-              Link
-            </a>
-            <a className="nav-link disabled" href="#">
-              Disabled
-            </a>
-          </nav>
-        </div>
-        <div>
-          <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <a className="navbar-brand" href="#">
-              Navbar
-            </a>
-            <button
-              className="navbar-toggler"
-              type="button"
-              data-toggle="collapse"
-              data-target="#navbarSupportedContent"
-              aria-controls="navbarSupportedContent"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <span className="navbar-toggler-icon"></span>
+          <nav className="navbar">
+            <button type="button" id="sidebarCollapse" className="btn btn-info">
+              <span>Toggle Sidebar</span>
             </button>
-
-            <div
-              className="collapse navbar-collapse"
-              id="navbarSupportedContent"
-            >
-              <ul className="navbar-nav mr-auto">
-                <li className="nav-item active">
-                  <a className="nav-link" href="#">
-                    Home <span className="sr-only">(current)</span>
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="#">
-                    Link
-                  </a>
-                </li>
-                <li className="nav-item dropdown">
-                  <a
-                    className="nav-link dropdown-toggle"
-                    href="#"
-                    id="navbarDropdown"
-                    role="button"
-                    data-toggle="dropdown"
-                    aria-haspopup="true"
-                    aria-expanded="false"
-                  >
-                    Dropdown{" "}
-                  </a>
-                  <div
-                    className="dropdown-menu"
-                    aria-labelledby="navbarDropdown"
-                  >
-                    <a className="dropdown-item" href="#">
-                      Action
-                    </a>
-                    <a className="dropdown-item" href="#">
-                      Another action
-                    </a>
-                    <div className="dropdown-divider"></div>
-                    <a className="dropdown-item" href="#">
-                      Something else here
-                    </a>
-                  </div>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link disabled" href="#">
-                    Disabled
-                  </a>
-                </li>
-              </ul>
-              <form className="form-inline my-2 my-lg-0">
-                <input
-                  className="form-control mr-sm-2"
-                  type="search"
-                  placeholder="Search"
-                  aria-label="Search"
-                />
-                <button
-                  className="btn btn-outline-success my-2 my-sm-0"
-                  type="submit"
-                >
-                  Search
-                </button>
-              </form>
-            </div>
           </nav>
         </div>
       </div>
     );
+    /*
+    return (
+      <div className="wrapper">
+        <nav id="sidebar">
+          <div>
+            <h3>SideBarHeader</h3>
+          </div>
+          <ul className="list-unstyled components">
+            <p>Dummy heading? But Paragraph</p>
+            <li className="hidden">
+              <a
+                href="#homeSubmenu"
+                data-toggle="collapse"
+                className="dropdown-toggle"
+              >
+                Home
+              </a>
+              <ul id="homeSubmenu" className="collapse list-unstyled">
+                <li>
+                  <a href="#">Home 1</a>
+                </li>
+                <li>
+                  <a href="#">Home 2</a>
+                </li>
+                <li>
+                  <a href="#">Home 3</a>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </nav>
+        <div id="content">
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="container-fluid">
+              <button
+                type="button"
+                id="sidebarCollapse"
+                className="btn btn-info"
+              >
+                <i className="fas fa-align-left"></i>
+                <span>Toggle Sidebar</span>
+              </button>
+            </div>
+          </nav>
+        </div>
+      </div>
+    )*/
   }
 }
