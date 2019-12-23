8f02852f5f78993383ad23fe8b8826e02a63f40a
Illkwon Ha <haik0925@gmail.com>
2019-12-23 12:32:27 -0800

    implemented basic sidebar

--- my_blog_app/src/BlogSideBar.tsx
+++ my_blog_app/src/BlogSideBar.tsx
@@ -3,32 +3,60 @@ import "./BlogSideBar.css";
 
 interface BlogSideBarProps {}
 
-interface BlogSideBarState {}
+interface BlogSideBarState {
+  categories: string[];
+  active: boolean;
+}
 
 export class BlogSideBar extends React.Component<
   BlogSideBarProps,
   BlogSideBarState
 > {
-  constructor(props: Readonly<{}>) {
+  constructor(props: BlogSideBarProps) {
     super(props);
 
-    this.state = {};
+    this.state = {
+      categories: ["Graphics", "CI", "Network", "Web", "Non-Programming"],
+      active: true
+    };
+  }
+
+  toggleSideBar = () => {
+    this.setState((prevState, _props) => {
+      return {
+        active: !prevState.active
+      };
+    });
+  };
+
+  categoryListItems() {
+    const items: JSX.Element[] = [];
+    for (let i = 0; i < this.state.categories.length; i++) {
+      items.push(
+        <li className="active" key={this.state.categories[i]}>
+          <a href="#">{this.state.categories[i]}</a>
+        </li>
+      );
+    }
+    return items;
   }
 
   render() {
     return (
       <div className="wrapper">
-        <nav id="sidebar">
-          <div>
-            <h3>SidebarHeader</h3>
-          </div>
-          <ul className="list-unstyled">
-            <li>hahaha</li>
+        <nav id="sidebar" className={this.state.active ? "active" : ""}>
+          <ul className="list-unstyled components">
+            {this.categoryListItems()}
           </ul>
         </nav>
         <div>
           <nav className="navbar">
-            <button type="button" id="sidebarCollapse" className="btn btn-info">
+            <button
+              type="button"
+              id="sidebarCollapse"
+              className="btn btn-info"
+              onClick={this.toggleSideBar}
+            >
               <span>Toggle Sidebar</span>
             </button>
           </nav>
